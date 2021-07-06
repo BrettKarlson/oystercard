3ecc2c6774319a1dc0e22e49ce298a78204af47f
Brett Karlson <brettjkarlson@gmail.com>
2021-07-06 08:58:31 +0100

    Files after challenge 7

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,26 @@
 require 'oystercard'
 
-describe Oystercard do
-  
+RSpec.describe Oystercard do
+    
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
+  describe 'changing the balance' do
+
+    it 'adds to the balance' do
+      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    end
+
+    it 'deducts from the balance' do
+      subject.top_up(30)
+      expect{ subject.deduct 5 }.to change{ subject.balance }.by -5
+    end
+
+    it 'raises an error if maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+    end
 end
